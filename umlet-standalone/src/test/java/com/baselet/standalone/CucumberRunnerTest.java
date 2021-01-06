7330eecdde01f2cfb3b7a8599b9a3bad61db5cbb
René Kok <rkok98@gmail.com>
2021-01-06 20:25:10 +0100

    Updated cucumber runner

---
+++ umlet-standalone/src/test/java/com/baselet/standalone/CucumberRunnerTest.java
@@ -0,0 +1,13 @@
+package com.baselet.standalone;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty"}, dryRun = true,
+        features = "src/test/resources/cucumber",
+        glue = "com/baselet/standalone")
+public class CucumberRunnerTest {
+}
