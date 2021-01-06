7b51c04f419fb129db9f11170647d0e7b34ce5b9
René Kok <rkok98@gmail.com>
2021-01-06 23:04:24 +0100

    Updated cucumber runner

--- umlet-standalone/src/test/java/com/baselet/standalone/CucumberRunnerTest.java
+++ umlet-standalone/src/test/java/com/baselet/standalone/CucumberRunnerTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty", "json:target/cucumber-reports"}, dryRun = true,
+        plugin = { "pretty", "html:target/cucumber-reports", "json:target/cucumber.json"},
         features = "src/test/resources/cucumber",
         glue = "com/baselet/standalone")
 public class CucumberRunnerTest {
